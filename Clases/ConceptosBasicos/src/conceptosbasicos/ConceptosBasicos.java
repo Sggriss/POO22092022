2503cd2c5e751dde6706473d235a39186c3fa735
Sggriss <grissellegarces69@aragon.unam.mx>
2022-03-01 12:51:09 -0600

    Sesión 1 marzo, Automovil

--- Clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
+++ Clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -4,6 +4,10 @@
  */
 package conceptosbasicos;
 
+import ico.Fes.Automovil;
+import ico.Fes.Silla;
+import java.awt.Color;
+
 /**
  *
  * @author grisssg26
@@ -16,6 +20,27 @@ public class ConceptosBasicos {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola de nuevo");
+        Silla s1 = new Silla();
+        System.out.println( s1 ); //Por defecto se invoca toString()
+        System.out.println(s1.toString() );
+        
+        
+        Automovil bocho=new Automovil();
+        System.out.println("El automovil es un: Bocho");
+        bocho.setMarca("VW"); //Asignación del estado "VW" al atributo Marca
+        System.out.println("Marca: " + bocho.getMarca() );
+        bocho.setSubMarca("Sedan"); //Asignación del estado "Sedan" al atributo SubMarca
+        System.out.println("SubMarca: " + bocho.getSubMarca() );
+        bocho.setModelo(1970); //Asignación del estado "1970" al atributo Modelo
+        System.out.println("Modelo: " + bocho.getModelo() );
+        bocho.setColor(Color.BLUE); //Asignación del estado "Azul-BLUE" al atributo color
+        System.out.println("Color: " + bocho.getColor() );
+        
+        Automovil miMustang = new Automovil("Ford", "Mustang", 2010, Color.yellow);
+        System.out.println(miMustang);
+        
+        Automovil miAcura = new Automovil("Acura", "NSX", 2013, Color.gray);
+        System.out.println(miAcura);
     }
     
 }
---
+++ Clases/ConceptosBasicos/src/ico/Fes/Automovil.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.Fes;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author grisssg26
+ */
+public class Automovil {
+    private String marca; //Los atributos van en notación lower camel case
+    private String subMarca; //Lower camel case, todas en minúsculas y a partir 
+                    //de la segunda palabra, la primer letra en mayúscula
+    private int modelo;
+    private Color color;
+
+    public Automovil() {
+    }
+
+    public Automovil(String marca, String subMarca, int modelo, Color color) {
+        this.marca = marca;
+        this.subMarca = subMarca;
+        this.modelo = modelo;
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getSubMarca() {
+        return subMarca;
+    }
+
+    public void setSubMarca(String subMarca) {
+        this.subMarca = subMarca;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() { //
+        return "Automovil{" + "marca=" + marca + ", subMarca=" + subMarca + ", modelo=" + modelo + ", color=" + color + '}';
+    }
+    
+    
+    
+}
